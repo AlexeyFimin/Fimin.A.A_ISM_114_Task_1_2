04b2354f7851413b3fadd2c4d3ae24beb4284656
AlexeyFimin <80840525+AlexeyFimin@users.noreply.github.com>
2021-10-15 00:13:44 +0300

    Исправил по другому не знаю как

--- task_1_num_8.cpp
+++ task_1_num_8.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cstring>
+#include <vector>
 using namespace std;
 
 
@@ -8,20 +8,29 @@ private:
     int p;
     int q;
     void reduce();
-    void addvalue (string& text, int value) const;
-
 public:
+    Rational(const int& p, const int& q = 1);
+    Rational(const Rational& other);
 
-    Rational() = default;
-    Rational(const int p, const int q = 1);
+    friend bool operator<(const Rational& first, const Rational& second);
+    friend bool operator==(const Rational& first, const Rational& second);
+    friend bool operator>(const Rational& first, const Rational& second);
 
-    void set_q (const int q)
-    {
-        return this->q;
+    friend Rational operator+(const Rational& first, const Rational& second);
+    friend Rational operator-(const Rational& first, const Rational& second);
+    friend Rational operator*(const Rational& first, const Rational& second);
+    friend Rational operator/(const Rational& first, const Rational& second);
+
+    friend void Print(const Rational& rational);
+
+};
+
+
+void Rational::reduce() {
+    if (this->p == 0) {
+        this->q = 1;
     }
-
-    void reduce() {
-       
+    else {
         int oldUp = abs(this->p);
         int oldDown = abs(this->q);
         while (oldDown != 0 && oldUp != 0) {
@@ -38,139 +47,111 @@ public:
         }
         this->p /= oldUp + oldDown;
         this->q /= oldUp + oldDown;
-    
     }
+}
 
 
-    void addvalue (string& text, int value) const {
-       
-        int ost;
-        while (value != 0) {
-            ost = value % 10;
-            temp += ost;
-            value -= ost;
-            value /= 10;
-        }
-        
-        string temp;
+Rational::Rational(const int& p, const int& q) {
+    this->p = p;
+    this->q = q;
+    this->reduce();
+}
 
-        for (int i = temp.size() - 1; i >= 0; --i) {
-            text += temp[i];
-        }
+
+Rational::Rational(const Rational& other) {
+    p = other.p;
+    q = other.q;
+}
+
+
+bool operator<(const Rational& first, const Rational& second) {
+    return ((double(first.p) / double(first.q)) < (double(second.p) / double(second.q)));
+}
+
+
+bool operator==(const Rational& first, const Rational& second) {
+    return (!(first < second) && !(second < first));
+}
+
+
+bool operator>(const Rational& first, const Rational& second) {
+    return !((first < second) || (first == second));
+}
+
+
+Rational operator+(const Rational& first, const Rational& second) {
+    if (second.p == 0) {
+        return first;
     }
-
-
-    stringstrem Rational::ToString() const {
-        string text;
-        addvalue (text, this->p);
-        text += '/';
-        addvalue (text, this->q);
-        return text;
+    else {
+        return Rational(first.p * second.q + first.q * second.p, first.q * second.q);
     }
+}
 
 
-    void Init(const int& p, const int& q) {
-        if (q != 0) {
-            this->p = p;
-            this->q = q;
-            reduce();
-        }
+Rational operator*(const Rational& first, const Rational& second) {
+    return Rational(first.p * second.p, first.q * second.q);
+}
+
+
+Rational operator-(const Rational& first, const Rational& second) {
+    if (second.p == 0) {
+        return first;
     }
-
-
-
-    Rational::Rational(const int& p, const int& q) {
-        this->p = p;
-        this->q = q;
-        this->reduce();
+    else {
+        return Rational(first.p * second.q - first.q * second.p, first.q * second.q);
     }
+}
 
 
-    bool Less(const Rational& second) const {
-        int ans1= (((double(this->p)*double(second.q)) / (double(this->q)*double(second.q)))
-        int ans2= (((double(second.p)*double(this->q)) / (double(second.q)*double(this->q)))
-
-        return  ans1 < ans2; 
+Rational operator/(const Rational& first, const Rational& second) {
+    if (second.p != 0) {
+        return Rational(first.p * second.q, first.q * second.p);
     }
+}
 
 
-    bool =Equal(const Rational& second) const {
-        return (!(this->Less(second)) && !(second.Less(*this)));
+void Print(const Rational& rational) {
+    cout << rational.p;
+    if (rational.q != 1) {
+        cout << '/' << rational.q;
     }
+    cout << endl;
+}
 
 
-    bool Grater(const Rational& second) const {
-        return !(this->Less(second) || this->Equal(second));
-    }
-
-
-    Rational Add(const Rational& second) const {
-        
-            return Rational(this->p * second.q + this->q * second.p, this->q * second.q);
-        
-    }
-
-
-    Rational Mul(const Rational& second) const {
-        return Rational(this->p * second.p, this->q * second.q);
-    }
-
-
-    Rational Sub(const Rational& second) const {
-       
-            return Rational(this->p * second.q - this->q * second.p, this->q * second.q);
-        
-    }
-
-
-    Rational Div(const Rational& second) const {
-        if (second.p != 0) {
-            Rational result(this->p * second.q, this->q * second.p);
-            return result;
-        }
-    }
-
-
-    void Display() const {
-        cout << this->p;
-        if (this->q != 1) {
-            cout << '/' << this->q;
-        }
-        cout << endl;
-    }
-
-
-    Rational MakeRational(const int& p, const int& q) {
-        return { p, q };
-    }
-};
 
 
 int main() {
     Rational a(1, 2);
-   
-    Rational c{};
-    c.Init(3, 5);
+    Rational b(2);
+    Rational c(b);
 
-    Rational d = MakeRational(8, 3);
+    vector<Rational> v;
+    for (int i = 0; i < 3; ++i) {
+        int p, q;
+        cin >> p >> q;
+        v.emplace_back(p, q);
+    }
+    for (int i = 0; i < 3; ++i) {
+        cout << "v[" << i << "] " << "is ";
+        Print(v[i]);
+    }
 
-    d.Display();
 
-    Rational result_1 = a.Add(b);
-    result_1.Display();
+    cout << "Add(a, b): ";
+    Print(a + b);
+    cout << "Sub(a, c): ";
+    Print(a - c);
+    cout << "Mul(a, b): ";
+    Print(a * b);
+    cout << "Div(a, c): ";
+    Print(a / c);
 
-    result_1 = a.Sub(c);
-    result_1.Display();
-
-    result_1 = a.Mul(b);
-    result_1.Display();
-
-    result_1 = b.Div(c);
-    result_1.Display();
-
-    cout << a.Less(b) << endl;;
-    cout << a.Equal(b) << endl;;
-    cout << c.Grater(b) << endl;
-
+    cout << "a < b is " << (a < b) << endl;
+    cout << "a == b is " << (a == b) << endl;
+    cout << "c > b is " << (c > b) << endl;
+     
+    cout << "Example of complicated expression " << ((((a + b) * c / b - a) < (b * (c + a) - c)) == ((c * a + b) > (b - a / c))); 
     return 0;
 }
